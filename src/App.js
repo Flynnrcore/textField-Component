737328dfcb93f128df6dc0dd2e82a474354a74dc
Vladislav Ulyanov <flynnrcore@yandex.ru>
2024-02-24 15:07:35 +0300

    feat: add examples TextField components

--- src/App.js
+++ src/App.js
@@ -4,36 +4,72 @@ import TextFieldComponent from './components/TextField.js';
 function App() {
   return (
     <div className="App">
-      <p>Пример компонента без переданных props</p>
-      <TextFieldComponent />
-      <p>Пример компонента с переданными props</p>
-      <TextFieldComponent
-        label="Custom label"
-        helperText="Custom helperText"
-        limitedSymbols
-      />
-      <p>Пример "multiline" компонента</p>
-      <TextFieldComponent
-        label="LabelTest"
-        helperText="Custom helperText with long description"
-        limitedSymbols
-        multiline
-      />
-
-      <p>Пример "error" компонента</p>
+      <section>
+        <p>Компонент без переданных props</p>
+        <TextFieldComponent />
+        <p>Компонент с переданными props</p>
+        <TextFieldComponent
+          label="Custom label"
+          helperText="Custom helperText"
+          limitedSymbols
+        />
+        <p>"Multiline" компонент</p>
+        <TextFieldComponent
+          label="LabelTest"
+          helperText="Custom helperText with long description"
+          limitedSymbols
+          multiline
+        />
+        <p>"Error" компонент</p>
         <TextFieldComponent
           label="errorLabel"
           helperText="error HelperText"
           limitedSymbols
           error
         />
-      <p>Пример "disabled" компонента</p>
+        <p>"Disabled" компонент</p>
         <TextFieldComponent
           label="disabled"
           helperText="disabled HelperText"
           limitedSymbols
           disabled
         />
+      </section>
+      <section>
+        <p>"Outlined" компонент без переданных props</p>
+        <TextFieldComponent variant="outlined" />
+        <p>"Outlined" компонент с переданными props</p>
+        <TextFieldComponent
+          label="Custom label"
+          helperText="Custom helperText"
+          limitedSymbols
+          variant="outlined"
+        />
+        <p>"Outlined multiline" компонент</p>
+        <TextFieldComponent
+          label="LabelTest"
+          helperText="Custom helperText with long description"
+          limitedSymbols
+          multiline
+          variant="outlined"
+        />
+        <p>"Outlined error" компонент</p>
+        <TextFieldComponent
+          label="errorLabel"
+          helperText="error HelperText"
+          limitedSymbols
+          variant="outlined"
+          error
+        />
+        <p>"Outlined disabled" компонент</p>
+        <TextFieldComponent
+          label="disabled"
+          helperText="disabled HelperText"
+          limitedSymbols
+          variant="outlined"
+          disabled
+        />
+      </section>
     </div>
   );
 }
